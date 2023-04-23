b26f3362a027ccbc2dca1587c9bd5b53c234a9ef
Niapoll <Kolya21082001@gmail.com>
2023-04-23 23:39:11 +0000

    Add SperPage component

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,19 @@ export default {
 }
 </script>
 
-<style></style>
+<style style lang="less">
+.noselect {
+  -webkit-touch-callout: none;
+  /* iOS Safari */
+  -webkit-user-select: none;
+  /* Safari */
+  -khtml-user-select: none;
+  /* Konqueror HTML */
+  -moz-user-select: none;
+  /* Old versions of Firefox */
+  -ms-user-select: none;
+  /* Internet Explorer/Edge */
+  user-select: none;
+  /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+}
+</style>
---
+++ src/views/Sper/components/AdCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="noselect ad-card-holder">
+    <img class="ad-card-holder__img" :src="image" draggable="false">
+    <div class="ad-card-holder__text">
+      <slot>Ad text placeholder</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdCard',
+  props: {
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@800');
+
+.ad-card-holder {
+  box-sizing: border-box;
+
+  max-width: 24.75vmin;
+  max-height: 37.5vmin;
+
+  padding: 2.5vmin;
+
+  background: #D9D9D9;
+  border: 1px solid black;
+
+  &__img {
+    width: 19.5vmin;
+    height: 18.75vmin;
+  }
+
+  &__text {
+    font-family: "Inter";
+    font-size: 1.7vmin;
+
+    padding: 0vmin 1.5vmin;
+
+    word-wrap: break-word;
+  }
+}
+</style>
